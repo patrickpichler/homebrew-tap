d165e09ebb393f2eca0c39e4865647927c565cfa
Patrick Pichler <git@patrickpichler.dev>
2023-10-08 16:24:56 +0200

    Initial commit

---
+++ Formula/path-formatter.rb
@@ -0,0 +1,14 @@
+class PathFormatter < Formula
+  desc "Small helper program to format a given path"
+  homepage "https://github.com/patrickpichler/path-formatter"
+  url "https://github.com/patrickpichler/path-formatter/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "067e0b86dddd47e6f2c3a113faa24108185fdf7aafed17629b81a991a7424dc6"
+  license "MIT"
+  head "https://github.com/patrickpichler/path-formatter.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+end
---
+++ README.md
@@ -0,0 +1,3 @@
+# My personal homebrew tap
+
+This homebrew tap contains brew formular for some of my private tools.
