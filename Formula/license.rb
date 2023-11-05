619e4e1c76a1fe86eef620982a5ed080630a4e04
Patrick Pichler <git@patrickpichler.dev>
2023-11-05 08:47:48 +0100

    feat: add license formula

---
+++ Formula/license.rb
@@ -0,0 +1,13 @@
+class License < Formula
+  desc "Command-line license text generator"
+  homepage "https://github.com/nishanths/license"
+  url "https://github.com/nishanths/license/archive/refs/tags/v5.0.4.tar.gz"
+  sha256 "66004bbc196405217161d378109517020f72fc3a71a532d13c25ff1b2c0055ae"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+end
