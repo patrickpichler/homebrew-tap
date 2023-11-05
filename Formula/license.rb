21159a16c98e93b2d3c3f7e6fc0129d6c34fd7d6
Patrick Pichler <13838138+patrickpichler@users.noreply.github.com>
2023-11-05 08:19:55 +0000

    license: add 5.0.4 bottle.

--- Formula/license.rb
+++ Formula/license.rb
@@ -5,6 +5,12 @@ class License < Formula
   sha256 "66004bbc196405217161d378109517020f72fc3a71a532d13c25ff1b2c0055ae"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/patrickpichler/homebrew-tap/releases/download/license-5.0.4"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, ventura: "00a32f0447954e0784424a2ac2da238c854946dd74e22b35b6b07f4476eb7d69"
+  end
+
   depends_on "go" => :build
 
   def install
