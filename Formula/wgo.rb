3f6f8bd398d5b06775ffb8ba63cae7f31a35d48a
Patrick Pichler <13838138+patrickpichler@users.noreply.github.com>
2024-01-05 19:23:22 +0000

    wgo: add 0.5.6 bottle.

--- Formula/wgo.rb
+++ Formula/wgo.rb
@@ -6,6 +6,12 @@ class Wgo < Formula
   license "MIT"
   head "https://github.com/bokwoon95/wgo.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/patrickpichler/homebrew-tap/releases/download/wgo-0.5.6"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, ventura: "b48642a90dcda3b7bbb2e6927d22023512dad4d6ce5001cba5824c7759c183da"
+  end
+
   depends_on "go" => :build
 
   def install
