53715dce3b14cdcb8320d4e756a6907e4425d95b
Patrick Pichler <13838138+patrickpichler@users.noreply.github.com>
2024-01-08 17:12:04 +0000

    swag: add 2.0.0-rc3 bottle.

--- Formula/swag.rb
+++ Formula/swag.rb
@@ -6,6 +6,11 @@ class Swag < Formula
     revision: "c7b796dcb851ffca7e16b18bc91aa3696f6dadf5"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/patrickpichler/homebrew-tap/releases/download/swag-2.0.0-rc3"
+    sha256 cellar: :any_skip_relocation, ventura: "5edb6f7bdc87f9250dc4930c2606af4d318051f279523bc5eb4e4583bc601d36"
+  end
+
   depends_on "go" => :build
 
   def install
